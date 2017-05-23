03d7ca84db04964a342ab7ccc4687a9caa684453
Bahar17 <baharak17@hotmail.com>
2017-05-23 16:57:36 +0100

    Update VenueSeaController.java
    I have made some changes.

--- src/main/java/com/java/VenueSeaController.java
+++ src/main/java/com/java/VenueSeaController.java
@@ -1,15 +1,15 @@
 package com.java;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -30,12 +30,15 @@ public class VenueSeaController{
           }
           
           public String objectMapper(String name) throws JsonProcessingException, IOException {
-        	  InputStream inputStream = new URL("https://api.foursquare.com/v2/venues/explore?near=London,London&type=venueless&client_id=RV5EB5MHKJS13TFRTPYVSMJXTOGP3YGAIUEL4J0JZBFDGJKI&client_secret=U5D3SOVI0L2ICX2ZPWW34HPE4MERPLMI1VI0WN5JTBZS2JRH&limit=10&v=20130815&query="+ name).openStream();     
-        	  ObjectMapper mapper = new ObjectMapper();
-	          JsonNode root = mapper.readTree(inputStream);
-	          String sjson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(root);
+        	  RestTemplate restTemplate = new RestTemplate();
+  	    	  String resourceUrl
+  	    	    = "https://api.foursquare.com/v2/venues/explore?near=London,London&type=venueless&client_id=RV5EB5MHKJS13TFRTPYVSMJXTOGP3YGAIUEL4J0JZBFDGJKI&client_secret=U5D3SOVI0L2ICX2ZPWW34HPE4MERPLMI1VI0WN5JTBZS2JRH&limit=10&v=20130815&query="+ name;
+  	    	  ResponseEntity<String> response
+  	    	    = restTemplate.getForEntity(resourceUrl + "/1", String.class);
+  	    	  String body = response.getBody();
+  	          ObjectMapper mapper = new ObjectMapper();
+                  JsonNode root = mapper.readTree(body);
+  	          String sjson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(root);
 	          return sjson;
           }
-
-
 }

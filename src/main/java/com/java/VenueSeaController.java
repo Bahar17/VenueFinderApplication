437e83da379c66c99d27f3f525c697f6beec0c22
Bahar17 <baharak17@hotmail.com>
2017-07-17 12:12:04 +0100

    Update VenueSeaController.java
    Updated 17-07-2017

--- src/main/java/com/java/VenueSeaController.java
+++ src/main/java/com/java/VenueSeaController.java
@@ -20,25 +20,31 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller 
 public class VenueSeaController{
 	
-          @RequestMapping("/search")
-	      public ModelAndView venueSea(HttpServletRequest req,HttpServletResponse res) throws IOException  {  
-	          String name=req.getParameter("name"); 
-	          String jsonoutput= objectMapper(name);
+         @RequestMapping("/search")
+	      public ModelAndView venueSea(HttpServletRequest req,HttpServletResponse res) throws IOException  { 
+        	  String name=req.getParameter("name");
+	          String jsonoutput= niceJsonBody(getJsonBody(getResourceUrl(name)));
 	          ModelAndView modelandView = new ModelAndView("mysearch");          
 	          modelandView.addObject("output", jsonoutput);
 	          return modelandView;                     
           }
           
-          public String objectMapper(String name) throws JsonProcessingException, IOException {
-        	  RestTemplate restTemplate = new RestTemplate();
+          private String getResourceUrl(String name) {  
   	    	  String resourceUrl
   	    	    = "https://api.foursquare.com/v2/venues/explore?near=London,London&type=venueless&client_id=RV5EB5MHKJS13TFRTPYVSMJXTOGP3YGAIUEL4J0JZBFDGJKI&client_secret=U5D3SOVI0L2ICX2ZPWW34HPE4MERPLMI1VI0WN5JTBZS2JRH&limit=10&v=20130815&query="+ name;
-  	    	  ResponseEntity<String> response
-  	    	    = restTemplate.getForEntity(resourceUrl + "/1", String.class);
-  	    	  String body = response.getBody();
-  	          ObjectMapper mapper = new ObjectMapper();
-                  JsonNode root = mapper.readTree(body);
-  	          String sjson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(root);
-	          return sjson;
+  	    	  return resourceUrl;
           }
+          private String getJsonBody(String res){
+        	  RestTemplate restTemplate = new RestTemplate();
+        	  ResponseEntity<String> response = restTemplate.getForEntity(res + "/1", String.class);
+	    	  String body = response.getBody();
+	    	  return body;    
+          }
+          private String niceJsonBody(String body) throws JsonProcessingException, IOException{
+        	  ObjectMapper mapper = new ObjectMapper();
+              JsonNode root = mapper.readTree(body);
+  	          String sjson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(root);
+  	          return sjson;
+          }
+
 }
